3b138d0afe450ac2730638f7aa330d8df548ba82
OKorzhov <Oleksandr.Korzhov@onseo.biz>
2019-02-08 11:55:42 +0200

    make base setup for server

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const config = require('./config');
 
 let app = express();
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.listen(config.SERVER_PORT, () => {
+    console.log(`Server listen on ${config.SERVER_PORT} port`);
+});
